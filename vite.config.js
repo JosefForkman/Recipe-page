6db1da91c57678877536602bd4dbac74ee6f9db5
JosefForkman <strand.vatten@outlook.com>
2024-03-17 15:21:24 +0100

    add vite-plugin-laravel-purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import purge from '@erbelion/vite-plugin-laravel-purgecss'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    css: {
-        preprocessorOptions: {
-            scss: {},
-        },
-    },
+    plugins: [
+        react(),
+        purge({
+            paths: [
+                "src/App.jsx",
+                "index.html",
+            ],
+        }),
+    ],
 });
